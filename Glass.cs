85362ea481826d54cb37672903352123034dc92d
Ben Havis <ben.havis1@gmail.com>
2021-05-08 17:18:09 -0400

    Add project files.

---
+++ Glass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RefillMyGlass
+{
+    public class Glass
+    {
+
+        public int LiquidLevel = 200;
+
+        public void Drink(int millileters)
+        {
+            LiquidLevel -= millileters;
+        }
+
+        public void Refill()
+        {
+            LiquidLevel = 200;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace RefillMyGlass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var glass = new Glass();
+
+            Console.WriteLine("Enter a command ");
+            var command = Console.ReadLine();
+            
+
+            while (command != "stop")
+            {
+                var splittedCommand = command.Split(' ');
+
+                switch (splittedCommand[0])
+                {
+                    case "print":
+                        Console.WriteLine($"This glass contains {glass.LiquidLevel}ml of liquid");
+                        break;
+                    case "drink":
+                        glass.Drink(int.Parse(splittedCommand[1]));
+                        break;
+
+
+                }
+                Console.WriteLine("Enter a command ");
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine("Program terminated");
+        }
+
+    }
+}
